a0d85b6c148c43921d999abe1df244a7806478f6
Ondrej Svoreň <xsvore01@stud.fit.vutbr.cz>
2018-05-16 10:48:26 +0200

    License added.

--- rzgenerator.py
+++ rzgenerator.py
@@ -7,6 +7,22 @@
 # Supervisor: prof. Ing. Adam Herout PhD.
 # School Year: 2017/18
 
+# This tool is used for creation of Synthetic Dataset Generator for Traffic Analysis.
+# Copyright (C) 2018  Ondrej Svoreň(xsvore01@stud.fit.vutbr.cz)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import numpy as np
 import io
 import os
